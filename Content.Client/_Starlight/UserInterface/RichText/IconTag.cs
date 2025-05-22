2b167b5763e55f29154a9b7ba1031512e45ae528
GabyChangelog <agentepanela2@gmail.com>
2025-05-22 20:29:27 +0000

    chore: Automatically update REUSE headers

--- Content.Client/_Starlight/UserInterface/RichText/IconTag.cs
+++ Content.Client/_Starlight/UserInterface/RichText/IconTag.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 cosmosgc <cosmoskitsune@hotmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 using Content.Shared.StatusIcon;
